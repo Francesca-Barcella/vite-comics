c60877454317c133d4c5222ab53f8b8a6473b658
Francesca-Barcella <pollon.fra@gmail>
2022-11-10 16:27:36 +0100

    comics - install sass + start template header

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,36 @@ export default {
 </script>
 
 <template>
-  <h1>testo prova</h1>
+  <header id="site-header">
+    header
+    <div class="log">
+      <img src="./assets/img/dc-logo.png" alt="">
+    </div>
+    <nav>
+      <a href="#">link 1</a>
+      <a href="#">link 2</a>
+      <a href="#">link 3</a>
+      <a href="#">link 4</a>
+      <a href="#">link 5</a>
+      <a href="#">link 6</a>
+      <a href="#">link 7</a>
+      <a href="#">link 8</a>
+      <a href="#">link 9</a>
+      <a href="#">link 10</a>
+
+    </nav>
+  </header>
+
+  <main id="site-main">
+    main
+  </main>
+
+  <footer id="site-footer">
+    footer
+  </footer>
+
 </template>
 
-<style scoped>
+<style lang="scss">
 
 </style>
