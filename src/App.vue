6cc06c766be823b4282dcc44957077fb4ec9d1e0
Francesca-Barcella <pollon.fra@gmail>
2022-11-10 16:38:02 +0100

    comics - create component siteheader

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,16 @@
 <script>
+import SiteHeader from './components/SiteHeader.vue'
 export default {
   name: 'Comics',
+  components:{
+    SiteHeader
+  }
 }
 </script>
 
 <template>
-  <header id="site-header">
-    header
-    <div class="log">
-      <img src="./assets/img/dc-logo.png" alt="">
-    </div>
-    <nav>
-      <a href="#">link 1</a>
-      <a href="#">link 2</a>
-      <a href="#">link 3</a>
-      <a href="#">link 4</a>
-      <a href="#">link 5</a>
-      <a href="#">link 6</a>
-      <a href="#">link 7</a>
-      <a href="#">link 8</a>
-      <a href="#">link 9</a>
-      <a href="#">link 10</a>
-
-    </nav>
-  </header>
+  <SiteHeader></SiteHeader>
+ 
 
   <main id="site-main">
     main
