3be99b1913a737a24627d64fda24a971a9aaf4e9
Francesca-Barcella <pollon.fra@gmail>
2022-11-13 18:05:14 +0100

    comics - cards dinamiche e inizio promps

---
+++ src/components/MainComicsCard.vue
@@ -0,0 +1,31 @@
+<script>
+import comics from '../data/dc-comics'
+
+export default {
+    name: 'Card'
+}
+</script>
+
+<template>
+
+
+   <div class="container py-5">
+            <div class="cards row">
+                <div class="card col-2" v-for="item in ComicsCards">
+                    <img :src="item.thumb"
+                        alt="">
+                    <h3>{{item.series}}</h3>
+                </div>
+            </div>
+        </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/_variables.scss' as *;
+.cards {
+        justify-content: space-between;
+        flex-wrap: wrap;
+
+  
+    }
+</style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,10 +1,12 @@
 <script>
 import MainBanner from './MainBanner.vue'
+import ComicsCards from './MainComicsCards.vue'
 
 export default {
     name: 'SiteMain',
-    components:{
+    components: {
         MainBanner,
+        ComicsCards
     }
 
 }
@@ -12,21 +14,71 @@ export default {
 
 <template>
     <main id="site-main">
-        <div class="container top py-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab cupiditate libero ullam et adipisci
-            necessitatibus, nihil architecto illo non, rem facere. Praesentium, debitis et. Tempora sit et ipsa
-            laudantium ut. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab cupiditate libero ullam et
-            adipisci necessitatibus, nihil architecto illo non, rem facere. Praesentium, debitis et. Tempora sit et ipsa
-            laudantium ut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab cupiditate libero ullam et
-            adipisci necessitatibus, nihil architecto illo non, rem facere. Praesentium, debitis et. Tempora sit et ipsa
-            laudantium ut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab cupiditate libero ullam et
-            adipisci necessitatibus, nihil architecto illo non, rem facere. Praesentium, debitis et. Tempora sit et ipsa
-            laudantium ut.Tempora sit et ipsa laudantium ut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
-            cupiditate libero ullam et adipisci necessitatibus, nihil architecto illo non, rem facere. Praesentium,
-            debitis et. Tempora sit et ipsa laudantium ut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
-            cupiditate libero ullam et adipisci necessitatibus, nihil architecto illo non, rem facere. Praesentium,
-            debitis et. Tempora sit et ipsa laudantium ut.
-        </div>
+        <ComicsCards />
+        <!-- <div class="container py-5">
+            <div class="cards row">
+                <div class="card col-2">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                        alt="">
+                    <h3>Action Comics</h3>
+                </div>
+                <div class="card col-2">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01"
+                        alt="">
+                    <h3>American Vampire 1976</h3>
+                </div>
+                <div class="card col-2">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb"
+                        alt="">
+                    <h3>Aquaman</h3>
+                </div>
+                <div class="card col-2">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                        alt="">
+                    <h3>Action Comics</h3>
+                </div>
+                <div class="card col-2">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01"
+                        alt="">
+                    <h3>American Vampire 1976</h3>
+                </div>
+                <div class="card col-2">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb"
+                        alt="">
+                    <h3>Aquaman</h3>
+                </div>
+                <div class="card col-2">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                        alt="">
+                    <h3>Action Comics</h3>
+                </div>
+                <div class="card col-2">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01"
+                        alt="">
+                    <h3>American Vampire 1976</h3>
+                </div>
+                <div class="card col-2">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb"
+                        alt="">
+                    <h3>Aquaman</h3>
+                </div>
+                <div class="card col-2">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                        alt="">
+                    <h3>Action Comics</h3>
+                </div>
+                <div class="card col-2">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01"
+                        alt="">
+                    <h3>American Vampire 1976</h3>
+                </div>
+                <div class="card col-2">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb"
+                        alt="">
+                    <h3>Aquaman</h3>
+                </div>
+            </div>
+        </div> -->
         <!-- serve per richiamare il banner nell'altro components -->
         <MainBanner />
 
